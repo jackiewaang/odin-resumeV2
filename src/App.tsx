ec2646395860a7dc9084084bdbf853af1516ad3d
jackiewaang <zjackiewang03@gmail.com>
2024-10-23 18:37:38 +0100

    change folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
     <>
-      
+      <Sidebar />
+      <Forms />
+      <Content />
     </>
   )
 }
